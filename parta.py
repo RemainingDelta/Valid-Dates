0e9d8ece4fdb0e558a0ac4507f44a691ea31a41d
Shiven Ajwaliya <146774012+RemainingDelta@users.noreply.github.com>
2024-12-05 01:45:50 -0500

    Version 3

--- parta.py
+++ parta.py
@@ -1,32 +1,52 @@
 from itertools import permutations
 import math
 
-def make_pairs_lists(n):
+# make the brother and sister lists [1, -1, 2, -2, ... , n, -n] 
+def make_sibling_lists(n):
     for i in range(0, 2 * n, 2): 
         brothers_list[i] = math.ceil((i + 1)/2)
     for i in range(1, 2 * n, 2): 
         brothers_list[i] = -(math.ceil((i + 1)/2))
     for i in range(0, 2 * n, 2): 
-        sister_list[i] = math.ceil((i + 1)/2)
+        sisters_list[i] = math.ceil((i + 1)/2)
     for i in range(1, 2 * n, 2): 
-        sister_list[i] = -(math.ceil((i + 1)/2))
+        sisters_list[i] = -(math.ceil((i + 1)/2))
 
+# returns the number of valid permutations
 def find_valid_perms():
+    
+    # set count of valid permutations to 0
     count = 0
-    perms = permutations(sister_list)
+   
+    # generates permutations for sisters_list
+    perms = permutations(sisters_list)
+    # for each perm in perms, add 1 to count if the perm is valid 
     for perm in perms:
+        # set is_valid to True, which assumes from the beginning that every perm is valid
         is_valid = True
+        
+        # for each even index check if the next index is the negative of the current index to indicate that the permutation is invalid
         for i in range(0, 2 * n, 2): 
             if (perm[i] == -(perm[i+1])):
                 is_valid = False
+        
+        # the permutation is still valid, then add one to the count
         if (is_valid):
-            # print (perm)
             count += 1
+    
+    # return count at the end with the total valid number of permutations for n pairs
     return count
 
+
+# set n to input value of the number of pairs 
 n = int (input ("How many pairs? "))
+
+# set brothers_list and sisters_list as lists of 0s of size 2n
 brothers_list = [0] * 2 * n
-sister_list = [0] * 2 * n
-make_pairs_lists(n)
-print(brothers_list)
-print(find_valid_perms())
+sisters_list = [0] * 2 * n
+
+# make the brother and sister lists 
+make_sibling_lists(n)
+
+# print the number of valid date permutations 
+print("Valid Dates:", find_valid_perms())
