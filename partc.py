aaafd9fda98994025f9dd167c6545e5a75dd676d
Shiven Ajwaliya <146774012+RemainingDelta@users.noreply.github.com>
2024-12-06 00:54:22 -0500

    Version 2

--- partc.py
+++ partc.py
@@ -7,18 +7,20 @@ def t(n):
     # set T(n) value for n = 1 as 0
     if n == 1:
         return 0
-    
-    # set permutations to 0
-    permutations = 0
 
     # T(n) = 4n * (n-1) * T(n-1) + 16 * nCr(n, 2) * (n-1) * T(n-2)
-    permutations += 4 * n * (n-1) * t(n-1) + 16 * math.comb(n, 2) * (n-1) * t(n-2)
+    permutations = 4 * n * (n-1) * valid_pairs[n-1] + 16 * math.comb(n, 2) * (n-1) * valid_pairs[n-2]
 
     # return permutations value
     return permutations
 
-# set n to input value of the number of pairs 
-n = int(input("How many pairs? "))
 
-# print T(n)
-print("T(n):", t(n))
+# T(n) list set to [] 
+valid_pairs = []
+
+# for n: [1-10], find t(n) and add it to valid_pairs list
+for n in range(11):
+    valid_pairs.append(t(n))
+
+# print valid_pairs
+print("T(n):", valid_pairs)
